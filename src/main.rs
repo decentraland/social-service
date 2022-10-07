c02e41d1b16838beae3878ffe87496e95079bf77
jmoguilevsky <jmoguilevsky@gmail.com>
2022-10-07 12:13:44 -0300

    fix

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use configuration::Config;
 mod components;
 mod configuration;
 
-#[get("/items")]
+#[get("/ping")]
 async fn ping(_app_data: Data<AppComponents>) -> HttpResponse {
     HttpResponse::Ok().json("pong")
 }
@@ -15,7 +15,14 @@ async fn ping(_app_data: Data<AppComponents>) -> HttpResponse {
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     let data = Data::new(AppComponents::default());
+    print!("data");
+
     let configuration = Config::new().unwrap();
+
+    print!(
+        "config {} {}",
+        configuration.server.host, configuration.server.port
+    );
 
     HttpServer::new(move || App::new().app_data(data.clone()).service(ping))
         .bind((configuration.server.host, configuration.server.port))?
