aa5961b7af34204d442cf6441debb64d7df1993d
jmoguilevsky <jmoguilevsky@gmail.com>
2022-10-18 17:54:38 -0300

    Feat: Add tracing (#11)
    * add tracing
    
    * add example of custom format and function lifecycle tracing
    
    * Update mod.rs
    
    * pr review

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::io;
 
-use crate::routes::health::health::health;
+use crate::metrics::initialize_metrics;
 use crate::routes::health::live::live;
+use crate::{components::tracing::init_telemetry, routes::health::health::health};
 use actix_web::{web::Data, App, HttpServer};
 use components::app::AppComponents;
 use configuration::Config;
 use log;
+use tracing_actix_web::TracingLogger;
 
 mod components;
 mod configuration;
@@ -17,6 +19,8 @@ async fn main() -> io::Result<()> {
     // logger initialization change implementation depending on need
     env_logger::init();
 
+    init_telemetry();
+
     let app_data = AppComponents::new().await;
     let data = Data::new(app_data);
     let configuration = Config::new().unwrap();
@@ -26,7 +30,8 @@ async fn main() -> io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(data.clone())
-            .wrap(metrics::initialize_metrics())
+            .wrap(initialize_metrics())
+            .wrap(TracingLogger::default())
             .service(live)
             .service(health)
     })
