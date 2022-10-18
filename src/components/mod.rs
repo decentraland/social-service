3c153db1415b9c4b64887d5aea00d30ad4e5197c
jmoguilevsky <jmoguilevsky@gmail.com>
2022-10-18 15:19:53 -0300

    add tracing

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,3 +1,4 @@
 pub mod app;
 pub mod health;
 pub mod numbers;
+pub mod tracing;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::io;
 
-use crate::routes::health::health::health;
+use crate::{routes::health::health::health, components::tracing::init_telemetry};
 use crate::routes::health::live::live;
 use actix_web::{web::Data, App, HttpServer};
 use components::app::AppComponents;
 use configuration::Config;
 use log;
+use tracing_actix_web::TracingLogger;
 
 mod components;
 mod configuration;
@@ -17,6 +18,8 @@ async fn main() -> io::Result<()> {
     // logger initialization change implementation depending on need
     env_logger::init();
 
+    init_telemetry();
+
     let app_data = AppComponents::new().await;
     let data = Data::new(app_data);
     let configuration = Config::new().unwrap();
@@ -27,6 +30,7 @@ async fn main() -> io::Result<()> {
         App::new()
             .app_data(data.clone())
             .wrap(metrics::initialize_metrics())
+            .wrap(TracingLogger::default())
             .service(live)
             .service(health)
     })
