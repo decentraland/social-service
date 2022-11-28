4cbe201e409a6d7d81b1c73382b03837553fbf35
jmoguilevsky <jmoguilevsky@gmail.com>
2022-11-28 19:09:42 -0300

    Revert "feat: add database component (#61)"
    This reverts commit d784fc792eff663aad8a2790b31241183c1f8e2e.

--- src/components/app.rs
+++ src/components/app.rs
@@ -1,43 +1,26 @@
-use crate::components::{
-    configuration::Config, database::DatabaseComponent, health::HealthComponent,
-    synapse::SynapseComponent,
-};
-
+use crate::components::{health::HealthComponent, synapse::SynapseComponent};
+use crate::configuration::Config;
 pub struct AppComponents {
     pub health: HealthComponent,
     pub synapse: SynapseComponent,
     pub config: Config,
-    pub db: DatabaseComponent,
 }
 
 impl AppComponents {
     pub async fn new(custom_config: Option<Config>) -> Self {
-        match env_logger::try_init() {
-            Err(_) => log::debug!("Logger already init"),
-            _ => {}
-        }
         // Initialize components
         let config =
             custom_config.unwrap_or_else(|| Config::new().expect("Couldn't read the configuratio"));
 
-        let mut health = HealthComponent::default();
+        let health = HealthComponent::default();
         let synapse = SynapseComponent::new(config.synapse.url.clone());
-        let mut db = DatabaseComponent::new(&config.db);
-        match db.run().await {
-            Err(err) => {
-                log::debug!("Error on running the DB: {:?}", err);
-                panic!("Unable to run the DB")
-            }
-            _ => {}
-        }
 
-        health.register_component(Box::new(db.clone()), "database".to_string());
+        // Register components to check health
 
         Self {
             config,
             health,
             synapse,
-            db,
         }
     }
 }
