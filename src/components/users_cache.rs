7ca49a2e9af99fb0cd8238314774e464c849d333
jmoguilevsky <jmoguilevsky@gmail.com>
2022-12-02 15:59:03 -0300

    changes

--- src/components/users_cache.rs
+++ src/components/users_cache.rs
@@ -63,3 +63,25 @@ impl UsersCacheComponent {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_divide() {
+        assert_eq!(divide_non_zero_result(10, 2), 5);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_any_panic() {
+        divide_non_zero_result(1, 0);
+    }
+
+    #[test]
+    #[should_panic(expected = "Divide result is zero")]
+    fn test_specific_panic() {
+        divide_non_zero_result(1, 10);
+    }
+}
