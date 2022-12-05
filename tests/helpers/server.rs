54f56b98c626b088b799c7c2659c5142bc74cc6c
lauti7 <lautigbustos@gmail.com>
2022-12-05 17:23:17 -0300

    tests: create random DBs for routes tests

--- tests/helpers/server.rs
+++ tests/helpers/server.rs
@@ -1,8 +1,13 @@
 use actix_web::{body::MessageBody, dev::ServiceFactory, App};
-use social_service::{components::configuration::Config, get_app_data, get_app_router};
+use social_service::{
+    components::configuration::{Config, Database},
+    get_app_data, get_app_router,
+};
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 
 pub fn get_configuration() -> Config {
-    let config = Config::new().expect("Couldn't read the configuration file");
+    let mut config = Config::new().expect("Couldn't read the configuration file");
+    config.db.name = uuid::Uuid::new_v4().to_string();
     config
 }
 
@@ -17,8 +22,41 @@ pub async fn get_app(
         InitError = (),
     >,
 > {
+    create_test_db(&config.db).await;
     let app_data = get_app_data(Some(config)).await;
     let app = get_app_router(&app_data);
 
     app
+}
+
+/// We need this to avoid conccurency issues in Tests
+pub async fn create_test_db(db_config: &Database) -> PgPool {
+    let url = format!(
+        "postgres://{}:{}@{}",
+        db_config.user, db_config.password, db_config.host
+    );
+
+    let mut connection = PgConnection::connect(url.as_str())
+        .await
+        .expect("Failed to connect to Postgres");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, db_config.name).as_str())
+        .await
+        .expect("Failed to create database");
+
+    let url = format!(
+        "postgres://{}:{}@{}/{}",
+        db_config.user, db_config.password, db_config.host, db_config.name
+    );
+
+    let pool = PgPool::connect(&url)
+        .await
+        .expect("Failed to connect to Postgress.");
+
+    sqlx::migrate!("./migrations")
+        .run(&pool)
+        .await
+        .expect("Failed to migrate DB");
+
+    pool
 }
