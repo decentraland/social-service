b60bddd18e12bda0f50d18577ef93ff58e6ab377
lauti7 <lautigbustos@gmail.com>
2022-12-07 17:48:59 -0300

    feat: add synapse fetch function to get user id

--- src/components/synapse.rs
+++ src/components/synapse.rs
@@ -9,11 +9,17 @@ pub struct SynapseComponent {
 }
 
 pub const VERSION_URI: &str = "/_matrix/client/versions";
+pub const WHO_AM_I_URI: &str = "/_matrix/client/v3/account/whoami";
 
 #[derive(Deserialize, Serialize)]
 pub struct VersionResponse {
     pub versions: Vec<String>,
     pub unstable_features: HashMap<String, bool>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct WhoAmIResponse {
+    pub user_id: String,
 }
 
 #[cfg_attr(test, faux::methods)]
@@ -30,6 +36,25 @@ impl SynapseComponent {
         let version_url = format!("{}{}", self.synapse_url, VERSION_URI);
         let result: Result<VersionResponse, String> = match reqwest::get(version_url).await {
             Ok(response) => match response.json::<VersionResponse>().await {
+                Ok(json) => Ok(json),
+                Err(err) => Err(err.to_string()),
+            },
+            Err(err) => Err(err.to_string()),
+        };
+
+        result
+    }
+
+    pub async fn who_am_i(&self, token: &str) -> Result<WhoAmIResponse, String> {
+        let who_am_i_url = format!("{}{}", self.synapse_url, WHO_AM_I_URI);
+        let client = reqwest::Client::new();
+        let result: Result<WhoAmIResponse, String> = match client
+            .get(who_am_i_url)
+            .header("authorization", token)
+            .send()
+            .await
+        {
+            Ok(response) => match response.json::<WhoAmIResponse>().await {
                 Ok(json) => Ok(json),
                 Err(err) => Err(err.to_string()),
             },
