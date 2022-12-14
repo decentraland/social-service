93a4d0ad3f25895ac7ebaf23c18ef020d3064d7a
lauti7 <lautigbustos@gmail.com>
2022-12-14 15:50:51 -0300

    synapse example

--- src/components/synapse.rs
+++ src/components/synapse.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
 
+#[cfg_attr(test, faux::create)]
 #[derive(Debug)]
 pub struct SynapseComponent {
     synapse_url: String,
@@ -11,10 +12,11 @@ pub const VERSION_URI: &str = "/_matrix/client/versions";
 
 #[derive(Deserialize, Serialize)]
 pub struct VersionResponse {
-    versions: Vec<String>,
-    unstable_features: HashMap<String, bool>,
+    pub versions: Vec<String>,
+    pub unstable_features: HashMap<String, bool>,
 }
 
+#[cfg_attr(test, faux::methods)]
 impl SynapseComponent {
     pub fn new(url: String) -> Self {
         if url.is_empty() {
@@ -35,5 +37,28 @@ impl SynapseComponent {
         };
 
         result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use faux::when;
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn should_get_an_empty_response() {
+        let mut synapse_mock = SynapseComponent::faux();
+
+        when!(synapse_mock.get_version).then(|_| {
+            Ok(VersionResponse {
+                versions: vec!["holaa".to_string()],
+                unstable_features: HashMap::new(),
+            })
+        });
+
+        let response = synapse_mock.get_version().await.unwrap();
+
+        assert_eq!(response.versions[0], "holaa")
     }
 }
