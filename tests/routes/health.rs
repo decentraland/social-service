f45f51f5175670b8ff189ebed65077f169b75c3c
lauti7 <lautigbustos@gmail.com>
2022-12-14 09:34:13 -0300

    tests: add mocking example for health component

--- tests/routes/health.rs
+++ tests/routes/health.rs
@@ -1,19 +1,42 @@
 #[cfg(test)]
 mod tests {
 
-    use crate::helpers::server::{get_app, get_configuration};
+    use std::collections::HashMap;
+
+    use crate::helpers::server::{
+        get_configuration, get_testing_app_data, MockHealth, OptionalComponents,
+    };
     use actix_web::test;
+    use social_service::{components::synapse::Synapse, get_app_router};
 
     #[actix_web::test]
     async fn test_index_get() {
         let config = get_configuration();
 
-        let app = test::init_service(get_app(config).await).await;
+        // Mocking Example for Health component
+        let mut health = MockHealth::default();
+
+        health.expect_register_component().times(2).return_const(());
+        health
+            .expect_calculate_status()
+            .once()
+            .returning(|| HashMap::new());
+
+        let components = OptionalComponents {
+            health: Some(health),
+            synapse: None,
+        };
+
+        let app_data = get_testing_app_data::<MockHealth, Synapse>(config, components).await;
+
+        let app = get_app_router(&app_data);
+
+        let app = test::init_service(app).await;
 
         let req = test::TestRequest::get().uri("/health/ready").to_request();
 
         let response = test::call_service(&app, req).await;
 
-        assert!(response.status().is_success())
+        assert!(response.status().is_success());
     }
 }
