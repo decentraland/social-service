5deb3e1c9c406af154efaf19f1b1a76cc8609be0
jmoguilevsky <jmoguilevsky@gmail.com>
2022-12-29 17:22:14 -0300

    pr review

--- src/entities/utils.rs
+++ src/entities/utils.rs
@@ -5,11 +5,11 @@ use crate::components::database::Executor;
 pub fn get_transaction_result_from_executor(
     executor_result: Option<Executor>,
 ) -> Option<Transaction<'_, Postgres>> {
-    match executor_result {
-        Some(executor) => match executor {
+    executor_result.map_or_else(
+        || None,
+        |executor| match executor {
             Executor::Transaction(transaction) => Some(transaction),
             Executor::Pool(_) => None,
         },
-        None => None,
-    }
+    )
 }
