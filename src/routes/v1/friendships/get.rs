00a5a83336ec4d8f4bc85d1f6821dcb3995e3198
jmoguilevsky <jmoguilevsky@gmail.com>
2023-01-18 17:11:41 -0300

    Update src/routes/v1/friendships/get.rs
    Signed-off-by: jmoguilevsky <jmoguilevsky@gmail.com>

--- src/routes/v1/friendships/get.rs
+++ src/routes/v1/friendships/get.rs
@@ -57,7 +57,6 @@ pub async fn get_user_friends(
 }
 
 fn has_permission(logged_user_id: &str, user_id: &str) -> bool {
-    println!("logged_user_id: {logged_user_id}, user_id: {user_id}");
 
     user_id.eq_ignore_ascii_case(logged_user_id)
 }
