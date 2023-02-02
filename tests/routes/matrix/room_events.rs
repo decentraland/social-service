67cdc071962c97ff5db9c3f1d49464f4ebebf9a4
Agustina Aldasoro <agusaldasoro@gmail.com>
2023-02-02 16:58:00 -0300

    fix tests

--- tests/routes/matrix/room_events.rs
+++ tests/routes/matrix/room_events.rs
@@ -36,13 +36,13 @@ mod tests {
                 RoomMember {
                     room_id: "a_room_id".to_string(),
                     r#type: "".to_string(),
-                    user_id: room_members.0,
+                    state_key: room_members.0,
                     social_user_id: None,
                 },
                 RoomMember {
                     room_id: "a_room_id".to_string(),
                     r#type: "".to_string(),
-                    user_id: room_members.1,
+                    state_key: room_members.1,
                     social_user_id: None,
                 },
             ],
