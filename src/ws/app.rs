c4f7a8ff8cc4712be0486e9424002c8222fd0c7b
Juli Milloch <millochjuli@gmail.com>
2023-03-23 19:09:57 +0100

    feat: get friends

--- src/ws/app.rs
+++ src/ws/app.rs
@@ -4,19 +4,29 @@ use dcl_rpc::{
 };
 
 // TODO!: suspicious imports ?) 0.0
-use crate::{ws::service::friendships_service, FriendshipsServiceRegistration, User};
+use crate::{
+    components::{
+        configuration::{Config, Database},
+        database::{DatabaseComponent, DatabaseComponentImplementation},
+    },
+    ws::service::friendships_service,
+    FriendshipsServiceRegistration, User,
+};
 
 pub async fn run_ws_transport() {
     let ws_server = WebSocketServer::new("127.0.0.1:8085");
 
     let mut connection_listener = ws_server.listen().await.unwrap();
 
-    let ctx = MyExampleContext {
+    let config = Config::new().expect("Couldn't read the configuration");
+
+    let ctx = MyContext {
         hardcoded_database: create_db(),
+        db: init_db_component(&config.db).await,
     };
 
     let mut server = RpcServer::create(ctx);
-    server.set_handler(|port: &mut RpcServerPort<MyExampleContext>| {
+    server.set_handler(|port: &mut RpcServerPort<MyContext>| {
         println!("Registering Rust Social WS Server");
         FriendshipsServiceRegistration::register_service(
             port,
@@ -54,6 +64,16 @@ fn create_db() -> Vec<User> {
     vec![user_1]
 }
 
-pub struct MyExampleContext {
+async fn init_db_component(db_config: &Database) -> DatabaseComponent {
+    let mut db = DatabaseComponent::new(db_config);
+    if let Err(err) = db.run().await {
+        log::debug!("Error on running the DB: {:?}", err);
+        panic!("Unable to run the DB")
+    }
+    db
+}
+
+pub struct MyContext {
     pub hardcoded_database: Vec<User>,
+    pub db: DatabaseComponent,
 }
--- src/ws/service/friendships_service.rs
+++ src/ws/service/friendships_service.rs
@@ -1,34 +1,62 @@
 use std::sync::Arc;
 
+use dcl_rpc::stream_protocol::Generator;
+
 use crate::{
-    ws::app::MyExampleContext, Empty, RequestEvents, ServerStreamResponse,
-    SharedFriendshipsService, SubscribeFriendshipEventsUpdatesResponse, UpdateFriendshipPayload,
-    UpdateFriendshipResponse, Users,
+    entities::friendships::FriendshipRepositoryImplementation, ws::app::MyContext, Empty,
+    RequestEvents, ServerStreamResponse, SharedFriendshipsService,
+    SubscribeFriendshipEventsUpdatesResponse, UpdateFriendshipPayload, UpdateFriendshipResponse,
+    User, Users,
 };
 
 pub struct MyFriendshipsService {}
 
 #[async_trait::async_trait]
-impl SharedFriendshipsService<MyExampleContext> for MyFriendshipsService {
+impl SharedFriendshipsService<MyContext> for MyFriendshipsService {
     async fn get_friends(
         &self,
         _request: Empty,
-        _context: Arc<MyExampleContext>,
+        context: Arc<MyContext>,
     ) -> ServerStreamResponse<Users> {
-        todo!()
+        // Get user_id from somewhere in the ether
+        let user_id = "";
+
+        // Look for friendships and build friend addresses list
+        let friendships = match &context.db.db_repos {
+            Some(repos) => {
+                let (friendships, _) = repos
+                    .friendships
+                    .get_user_friends(&user_id, true, None)
+                    .await;
+                match friendships {
+                    Err(_) => todo!(),
+                    Ok(it) => it,
+                }
+            }
+            None => todo!(),
+        };
+
+        let (generator, generator_yielder) = Generator::create();
+        let mut users = Users::default();
+        for friend in &friendships {
+            let user = User {
+                address: friend.address_1.clone(),
+            };
+            users.users.push(user);
+        }
+        generator_yielder.r#yield(users).await.unwrap();
+
+        generator
     }
-    async fn get_request_events(
-        &self,
-        _request: Empty,
-        _context: Arc<MyExampleContext>,
-    ) -> RequestEvents {
+
+    async fn get_request_events(&self, _request: Empty, _context: Arc<MyContext>) -> RequestEvents {
         todo!()
     }
 
     async fn update_friendship_event(
         &self,
         _request: UpdateFriendshipPayload,
-        _context: Arc<MyExampleContext>,
+        _context: Arc<MyContext>,
     ) -> UpdateFriendshipResponse {
         todo!()
     }
@@ -36,7 +64,7 @@ impl SharedFriendshipsService<MyExampleContext> for MyFriendshipsService {
     async fn subscribe_friendship_events_updates(
         &self,
         _request: Empty,
-        _context: Arc<MyExampleContext>,
+        _context: Arc<MyContext>,
     ) -> ServerStreamResponse<SubscribeFriendshipEventsUpdatesResponse> {
         todo!()
     }
