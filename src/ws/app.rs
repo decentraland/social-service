d500b6cf9dd1f82543fb4d08141775633b01e033
Agustina Aldasoro <agusaldasoro@gmail.com>
2023-03-31 19:37:47 -0300

    clippy

--- src/ws/app.rs
+++ src/ws/app.rs
@@ -47,10 +47,9 @@ pub async fn run_ws_transport(app_components: Arc<AppComponents>) -> tokio::task
         }
     });
 
-    let rpc_server_handle = tokio::spawn(async move {
+    tokio::spawn(async move {
         server.run().await;
-    });
-    rpc_server_handle
+    })
 }
 
 pub struct SocialContext {
