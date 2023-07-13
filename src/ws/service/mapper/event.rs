ca4fc368fb0c359b320246ddd50bc47911374426
Agustina Aldasoro <agusaldasoro@users.noreply.github.com>
2023-07-13 18:34:57 -0300

    Feat/connection total metric (#268)
    * feat: connected clients
    
    * feat: collect with increment_connected_clients
    
    * feat: collect with decrement_connected_clients
    
    * feat: counter updates sent on subscription (#256)
    
    * feat: histogram procedure call size (#257)
    
    * feat: histogram procedure call duration (#258)
    
    * review: rename collector func to `record_friendship_event_updates_sent`
    
    * review: wip metric for the size of the outgoing payload
    
    * review: wip metric for the size of the outgoing payload
    
    * review: wip metric for the size of the outgoing payload
    
    * review: metric for the size of the outgoing payload
    
    * chore: nits
    
    * review: rename
    
    * review: move collectors into metrics impl
    
    * feat: histogram connection duration (#260)
    
    * feat: connection duration
    
    * feat: connection duration
    
    * chore: clippy
    
    * Send connection duration as Histogram and not vector
    
    * Remove transport once
    
    ---------
    
    Signed-off-by: Agustina Aldasoro <agusaldasoro@users.noreply.github.com>
    Co-authored-by: Agustina Aldasoro <agusaldasoro@users.noreply.github.com>
    Co-authored-by: Agustina Aldasoro <agusaldasoro@gmail.com>
    
    * feat: Separate concerns in stream methods (#265)
    
    * fix build
    
    * Fix metric
    
    * Fix deadlock
    
    * release read lock
    
    * use same lock to read and write
    
    * wip
    
    * use write
    
    * fix deadlock
    
    ---------
    
    Signed-off-by: Agustina Aldasoro <agusaldasoro@users.noreply.github.com>
    Co-authored-by: Juli Milloch <millochjuli@gmail.com>
    Co-authored-by: Juli Milloch <42394626+Julieta11@users.noreply.github.com>

--- src/ws/service/mapper/event.rs
+++ src/ws/service/mapper/event.rs
@@ -144,6 +144,7 @@ pub fn update_request_as_event_payload(
     Ok(event_payload)
 }
 
+///
 pub fn event_response_as_update_response(
     request: UpdateFriendshipPayload,
     result: EventResponse,
@@ -232,6 +233,7 @@ pub fn event_response_as_update_response(
     Ok(update_response)
 }
 
+/// Maps a `FriendshipEventPayload` to an `Event` struct.
 pub fn update_friendship_payload_as_event(
     payload: FriendshipEventPayload,
     from: &str,
@@ -245,5 +247,18 @@ pub fn update_friendship_payload_as_event(
         })
     } else {
         Err(CommonError::Unknown("".to_owned()))
+    }
+}
+
+/// Maps a `FriendshipEventPayload` to an `FriendshipEvent` struct.
+pub fn parse_event_payload_to_friendship_event(
+    payload: FriendshipEventPayload,
+) -> Option<FriendshipEvent> {
+    match payload.body? {
+        friendship_event_payload::Body::Request(_) => Some(FriendshipEvent::REQUEST),
+        friendship_event_payload::Body::Accept(_) => Some(FriendshipEvent::ACCEPT),
+        friendship_event_payload::Body::Reject(_) => Some(FriendshipEvent::REJECT),
+        friendship_event_payload::Body::Delete(_) => Some(FriendshipEvent::DELETE),
+        friendship_event_payload::Body::Cancel(_) => Some(FriendshipEvent::CANCEL),
     }
 }
