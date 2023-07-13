d38d5f6c3bb553e23021ef411af1b0afe4974a9d
Agustina Aldasoro <agusaldasoro@gmail.com>
2023-07-13 17:06:24 -0300

    restore query

--- src/entities/queries.rs
+++ src/entities/queries.rs
@@ -1,23 +1,47 @@
 // This query retrieves the intersecition of friends between two users
-pub const MUTUALS_FRIENDS_QUERY: &str = "WITH friendsA AS (
-  SELECT CASE
-           WHEN LOWER(address_1) = LOWER($1) THEN address_2
-           ELSE address_1
-         END AS address
-  FROM friendships
-  WHERE LOWER(address_1) = LOWER($1) OR LOWER(address_2) = LOWER($1)
-),
-friendsB AS (
-  SELECT CASE
-           WHEN LOWER(address_1) = LOWER($2) THEN address_2
-           ELSE address_1
-         END AS address
-  FROM friendships
-  WHERE LOWER(address_1) = LOWER($2) OR LOWER(address_2) = LOWER($2)
+pub const MUTUALS_FRIENDS_QUERY: &str = "WITH friendsA as (
+  SELECT
+    CASE
+      WHEN LOWER(address_1) = LOWER($1) then address_2
+      else address_1
+    end as address
+  FROM
+    (
+      SELECT
+        f_a.*
+      from
+        friendships f_a
+      where
+        (
+          LOWER(f_a.address_1) = LOWER($1)
+          or LOWER(f_a.address_2) = LOWER($1)
+        )
+    ) as friends_a
 )
-SELECT address
-FROM friendsA
-WHERE address IN (SELECT address FROM friendsB);";
+SELECT
+  address
+FROM
+  friendsA f_b
+WHERE
+  address IN (
+    SELECT
+      CASE
+        WHEN LOWER(address_1) = LOWER($2) then address_2
+        else address_1
+      end as address_a
+    FROM
+      (
+        SELECT
+          f_b.*
+        from
+          friendships f_b
+        where
+          (
+            LOWER(f_b.address_1) = LOWER($2)
+            or LOWER(f_b.address_2) = LOWER($2)
+          )
+      ) as friends_b
+  );";
 
 /// This query fetches the rows where the lastest event of a friendship_id is a REQUEST,
 /// and either address_1 or address_2 is equal to the given user's address.
