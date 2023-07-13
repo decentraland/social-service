448b0bf704f30388860e0f1e0e8132afc117eefb
Agustina Aldasoro <agusaldasoro@gmail.com>
2023-07-13 18:25:01 -0300

    fix deadlock

--- src/ws/service/friendships_service.rs
+++ src/ws/service/friendships_service.rs
@@ -487,31 +487,18 @@ impl FriendshipsServiceServer<SocialContext, RPCFriendshipsServiceError> for MyF
                 );
 
                 // Attach social_id to the context by transport_id
-                let mut write = context.server_context.transport_context.write().await;
-                let social_transport_context = write.get(&context.transport_id);
-
-                match social_transport_context {
-                    Some(ctx) => {
-                        write.insert(
-                            context.transport_id,
-                            SocialTransportContext {
-                                address: Address(user_id.social_id.to_string()),
-                                connection_ts: ctx.connection_ts,
-                            },
-                        );
-                    }
-                    None => {
+                let mut transport_context = context.server_context.transport_context.write().await;
+                transport_context
+                    .entry(context.transport_id)
+                    .and_modify(|e| e.address = Address(user_id.social_id.to_string()))
+                    .or_insert_with(|| {
                         log::warn!("This code should be unreachable");
                         // This should never happen
-                        write.insert(
-                            context.transport_id,
-                            SocialTransportContext {
-                                address: Address(user_id.social_id.to_string()),
-                                connection_ts: Instant::now(),
-                            },
-                        );
-                    }
-                }
+                        SocialTransportContext {
+                            address: Address(user_id.social_id.to_string()),
+                            connection_ts: Instant::now(),
+                        }
+                    });
 
                 // Attach generator to the context by user_id
                 context
