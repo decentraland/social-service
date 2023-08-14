3cf67618be3b5da62295f79f3a50c26ea5645b8c
Agustina Aldasoro <agusaldasoro@gmail.com>
2023-08-14 15:29:59 +0100

    replace comment

--- src/synapse/synapse_handler.rs
+++ src/synapse/synapse_handler.rs
@@ -24,7 +24,8 @@ fn build_room_local_alias(acting_user: &str, second_user: &str) -> String {
     addresses.join("+")
 }
 
-/// When accepting a friend request, then the room invitation needs to be accepted too. Check out [here](https://spec.matrix.org/v1.3/client-server-api/#room-membership) to find out more about room membership and permissions based on the state in which a user may be.
+/// When interacting with a friend, then you need to join the room if you hadn't joined it yet.
+/// Check out [here](https://spec.matrix.org/v1.3/client-server-api/#room-membership) to find out more about room membership and permissions based on the state in which a user may be.
 pub async fn accept_room_invitation<'a>(
     token: &str,
     room_id: &str,
